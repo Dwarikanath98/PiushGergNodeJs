e429a085403b701ea2d3c97ad6f829c109aa7aaf
Dwarikanath98 <dwarikanathbiswal0@gmail.com>
2025-01-28 18:32:45 +0530

    my third commit

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("hello ! Dwarika")
+//const {add, sub} = require('./math.js'); // for access math files 
+const math = require('./math'); // for access math files
+//console.log("math value is ", add(2,5), sub(4,5)); this is one way to calculate the value
+
+console.log("math value is ",math);
+console.log("math value is ",math.add(2,5),math.sub(4,5));
